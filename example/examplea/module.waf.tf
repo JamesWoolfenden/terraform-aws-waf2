dca3387f37cdf56b3b135579a5906c2a5cc30167
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:17:28 +0100

    fix: skip CKV2_AWS_64 on kms key resources

--- example/examplea/module.waf.tf
+++ example/examplea/module.waf.tf
@@ -3,6 +3,7 @@ module "waf" {
   kms_key_arn = aws_kms_key.waf.arn
 }
 resource "aws_kms_key" "waf" {
+  # checkov:skip=CKV2_AWS_64: For example only, key policy managed via IAM
   enable_key_rotation = true
   policy              = <<POLICY
 {
